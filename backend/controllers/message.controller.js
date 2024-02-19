84c12d334ee9ba123a11d08e8293b5088e4aada9
Gautam Bisht <bishtgautam012@gmail.com>
2024-02-19 15:34:16 +0530

    api

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -26,10 +26,35 @@ export const sendMessage = async (req, res) => {
 
     if (newMessage) {
       conversation.messages.push(newMessage._id);
-      await conversation.save();
     }
 
+    // Socket io Functionality will go here
+
+    // await conversation.save();
+    // await newMessage.save();
+
+    await Promise.all([conversation.save(), newMessage.save()]);
+
     res.status(201).json(newMessage);
+  } catch (error) {
+    console.log("Error in send Message controller:", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages");
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+    res.status(200).json(messages);
   } catch (error) {
     console.log("Error in send Message controller:", error.message);
     res.status(500).json({ error: "Internal server error" });
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+
 import { connect } from "./db/connect.js";
 
 dotenv.config();
@@ -15,6 +18,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+app.use("/api/users", usersRoutes);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
